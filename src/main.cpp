3e3e42da99023cf382b56583f4e7a95fe2d4eda2
7hat3eird0ne <7hat3eird0ne@gmail.com>
2025-07-14 13:41:03 +0200

    removed argc/argv temporarily

--- src/main.cpp
+++ src/main.cpp
@@ -213,8 +213,7 @@ void backward(const CharacterSet& characterSet) {
 }
 
 
-int main(int argc, char* argv[]) {
-    //if (argc == 1)
+int main() {
     CharacterSet characterSet {};
     while (true) {
         char command {getCommand()};
